1c2915f8f2d422ac8ff4209cf5806ad079cede9a
Ian Lyell <ian.lyell@yahoo.com>
2023-05-24 10:35:45 -0500

    Add test for course #initialize method

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,24 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+    describe '#initialize' do
+        it 'exist, and takes 2 arguments' do
+            course = Course.new("Calculus", 2)
 
+            expect(couse).to be_a(Course)
+        end
+        
+        it 'stores arguments as a name and a integer' do
+            course = Course.new("Calculus", 2)
+
+            expect(couse.name).to eq("Calculus")
+            expect(course.capacity).to eq(2)
+        end
+
+        it 'Hold an array of students that starts empty' do
+            course = Course.new("Calculus", 2)
+
+            expect(couse.students).to eq([])
+        end
+    end
 end
