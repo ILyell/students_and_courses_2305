89488442bebdddc212bd3ffc25a3a3169933b127
Ian Lyell <ian.lyell@yahoo.com>
2023-05-24 10:13:36 -0500

    Pass student initialize test

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
 class Student
- 
+
+    attr_reader :name, :age, :scores
+
+    def initialize(student_hash)
+        @name = student_hash[:name]
+        @age = student_hash[:age]
+        @scores = []
+    end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,20 +4,20 @@ require './lib/student'
 RSpec.describe Student do
     describe '#initialize' do
         it 'exist, and takes a hash attribute on initialize' do
-            student = Student.new({name: "Morgan", agçe: 21})
+            student = Student.new({name: "Morgan", age: 21})
 
             expect(student).to be_a(Student)
         end
 
         it 'Stores a name and an age from the hash passed to it' do
-            student = Student.new({name: "Morgan", agçe: 21})
+            student = Student.new({name: "Morgan", age: 21})
             
             expect(student.name).to eq("Morgan")
             expect(student.age).to eq(21)
         end
 
         it 'Starts with a @scores as an empty array' do
-            student = Student.new({name: "Morgan", agçe: 21})
+            student = Student.new({name: "Morgan", age: 21})
             
             expect(student.scores).to eq([])
             
