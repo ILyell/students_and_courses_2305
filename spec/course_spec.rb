ff71038dd83f9d9fb7cbd4d299b1d39e91ea12e9
Ian Lyell <ian.lyell@yahoo.com>
2023-05-24 10:37:33 -0500

    Pass course #initialize test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe Course do
         it 'exist, and takes 2 arguments' do
             course = Course.new("Calculus", 2)
 
-            expect(couse).to be_a(Course)
+            expect(course).to be_a(Course)
         end
         
         it 'stores arguments as a name and a integer' do
             course = Course.new("Calculus", 2)
 
-            expect(couse.name).to eq("Calculus")
+            expect(course.name).to eq("Calculus")
             expect(course.capacity).to eq(2)
         end
 
         it 'Hold an array of students that starts empty' do
             course = Course.new("Calculus", 2)
 
-            expect(couse.students).to eq([])
+            expect(course.students).to eq([])
         end
     end
 end
